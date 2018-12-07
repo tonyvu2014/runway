ae32892c64673bbc2120d6a0e4229760df541fee
Tony Vu <tonyvu2008@gmail.com>
2018-12-07 17:38:50 +1100

    Update security config

--- src/main/java/com/industrieit/catwalk/runway/config/WebSecurityConfig.java
+++ src/main/java/com/industrieit/catwalk/runway/config/WebSecurityConfig.java
@@ -11,8 +11,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 .antMatchers("/actuator/**").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().permitAll();
     }
 }
