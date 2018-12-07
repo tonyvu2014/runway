0cf12ed40fdd585e26f08c9b6c5b32da83694cf4
Tony Vu <tonyvu2008@gmail.com>
2018-12-07 17:39:09 +1100

    Update controller

--- src/main/java/com/industrieit/catwalk/runway/controller/CustomerController.java
+++ src/main/java/com/industrieit/catwalk/runway/controller/CustomerController.java
@@ -1,23 +1,39 @@
 package com.industrieit.catwalk.runway.controller;
 
+import com.industrieit.catwalk.runway.dto.CustomerDTO;
+import com.industrieit.catwalk.runway.exception.DataNotFoundException;
 import com.industrieit.catwalk.runway.model.Customer;
 import com.industrieit.catwalk.runway.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping(value="api")
 public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
 
-    @GetMapping("/customers")
+    @GetMapping(value="customers")
     public List<Customer> getAllCustomers() {
         return customerService.getAllCustomers();
+    }
+
+    @GetMapping(value="customer")
+    public Customer getCustomer(String email) throws DataNotFoundException {
+        return customerService.getCustomer(email);
+    }
+
+    @PostMapping(value="customer")
+    @ResponseBody
+    public Customer addCustomer(@RequestBody CustomerDTO customerDTO) {
+        return customerService.addCustomer(customerDTO);
+    }
+
+    @DeleteMapping(value="customer")
+    public void deleteCustomer(String email) {
+        customerService.deleteCustomer(email);
     }
 }
