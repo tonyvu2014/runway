9b296cb0d4effa203fe360e67de0ac21802a2671
Tony Vu <tonyvu2008@gmail.com>
2018-12-07 17:37:14 +1100

    Update migration scripts

--- src/main/resources/db/migration/V2__insert_first_customer.sql
+++ src/main/resources/db/migration/V2__insert_first_customer.sql
@@ -1,2 +1,2 @@
-INSERT INTO runway.customer(id, first_name, last_name, email, date_of_birth)
-VALUES (1, 'Tony', 'Vu', 'tony.vu@runway.com', '1985-05-26');
+INSERT INTO runway.customer(first_name, last_name, email, date_of_birth)
+VALUES ('Tony', 'Vu', 'tony.vu@runway.com', '1985-05-26');
