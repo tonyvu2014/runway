abe7ac29ebc45bbb4b61265de5694c66b670fd11
Tony Vu <tonyvu2008@gmail.com>
2018-12-07 17:39:36 +1100

    Update Respository

--- src/main/java/com/industrieit/catwalk/runway/dao/CustomerRepository.java
+++ src/main/java/com/industrieit/catwalk/runway/dao/CustomerRepository.java
@@ -2,8 +2,18 @@ package com.industrieit.catwalk.runway.dao;
 
 import com.industrieit.catwalk.runway.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
 public interface CustomerRepository  extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByEmail(String email);
+
+    @Modifying
+    @Transactional
+    void deleteByEmail(String email);
 }
